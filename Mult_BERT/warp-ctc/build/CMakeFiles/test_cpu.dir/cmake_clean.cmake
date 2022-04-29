f2a49461911813535a9f96ea44dfd9b35a41acb7
cindy <cindyyting@163.com>
2022-04-29 10:36:19 +0800

    first commit

---
+++ Mult_BERT/warp-ctc/build/CMakeFiles/test_cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cpu.dir/tests/test_cpu.cpp.o"
+  "CMakeFiles/test_cpu.dir/tests/random.cpp.o"
+  "test_cpu.pdb"
+  "test_cpu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Mult_BERT/warp-ctc/build/CMakeFiles/warpctc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warpctc.dir/src/ctc_entrypoint.cpp.o"
+  "libwarpctc.pdb"
+  "libwarpctc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/warpctc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
